1efb1dc75d8da05cc5a4da6e1f41b87f9eb7f79f
Ashish Malviya <60316270+justashish1@users.noreply.github.com>
2024-07-01 18:26:50 +0530

    Update STARENGTSANALYTICSAPP.py

--- STARENGTSANALYTICSAPP.py
+++ STARENGTSANALYTICSAPP.py
@@ -37,7 +37,7 @@ def load_logo(filename):
 # Developer info at the bottom left
 st.markdown("""
         <div class='developer-info'>
-            Developer Name : Ashish Malviya, E-mail : info@starengts.com, Website: www.starengts.com<br>
+            Developer Name : Ashish Malviya E-mail : info@starengts.com Website: www.starengts.com<br>
         </div>
     """, unsafe_allow_html=True)
 
