6860b08433c0cbb9cc31aa8d658c42dc34882c4b
Ashish Malviya <60316270+justashish1@users.noreply.github.com>
2024-07-11 12:58:26 +0530

    Update STARENGTSANALYTICSAPP.py 1.0.20

--- STARENGTSANALYTICSAPP.py
+++ STARENGTSANALYTICSAPP.py
@@ -40,7 +40,7 @@ def load_logo(filename):
 # Developer info at the bottom left
 st.markdown("""
     <div class='developer-info'>
-        Developer Name : Ashish Malviya Version 1.0.20<br>
+        Developer : www.starengts.com This is the copyright starengts property, software  Version 1.0.20<br>
     </div>
 """, unsafe_allow_html=True)
 
