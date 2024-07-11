5625c27c9c954b6dc67b7cc0e435205e3f464f4a
Ashish Malviya <60316270+justashish1@users.noreply.github.com>
2024-07-11 13:24:10 +0530

    Update STARENGTSANALYTICSAPP.py 1.0.20

--- STARENGTSANALYTICSAPP.py
+++ STARENGTSANALYTICSAPP.py
@@ -41,7 +41,9 @@ def load_logo(filename):
 st.markdown("""
     <div class='developer-info'>
         Developer : www.starengts.com, Software version 1.0.20<br>
-        Starengts or streamlit will not store or use the data uploaded, The application use RAM to store temporary data and removal when reaploaded or refresh the page.<br>
+        Starengts or streamlit will not store or use the data uploaded. <br>
+        The application use RAM to store temporary data and removal  <br>
+        when reaploaded or refresh the page. <br>
         Copyright © 2021 Starengts - All Rights Reserved for website.
     </div>
 """, unsafe_allow_html=True)
