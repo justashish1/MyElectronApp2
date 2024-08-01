b6c70552fd755657cec20ab811a58b2d6cc5dedd
Ashish Malviya <60316270+justashish1@users.noreply.github.com>
2024-08-01 18:12:01 +0530

    Update STARENGTSANALYTICSAPP.py 1.0.21

--- STARENGTSANALYTICSAPP.py
+++ STARENGTSANALYTICSAPP.py
@@ -208,12 +208,10 @@ def authenticate(username, password):
 # Load data from uploaded file
 @st.cache_data
 def load_data(uploaded_file):
-    if uploaded_file.name.endswith(('.xlsx', '.xls', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
+    if uploaded_file.name.endswith('.xlsx'):
         return pd.read_excel(uploaded_file)
-    elif uploaded_file.name.endswith('.json'):
-        return pd.read_json(uploaded_file)
     else:
-        return pd.read_csv(uploaded_file)e
+        return pd.read_csv(uploaded_file)
 
 # Prompt the user to select the datetime column
 def select_datetime_column(df):
